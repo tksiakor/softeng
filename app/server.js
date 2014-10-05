c693b3f197a9f36ced9cc30bd77644864863d380
Kpetermeni Siakor <tksiakor@gmail.com>
2014-10-05 20:10:28 +0000

    Starting a new branch

---
+++ app/server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+
+http.createServer(function(request,response){
+	console.log("URI"+request.url);
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8000);
+console.log("Listening to port 8000");
+
+var ical = require('ical');
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+ical.fromURL('http://ashesi.edu.gh/resources/events.html?task=ical.download&id=181', {}, function(err, data) {
+      for (var k in data){
+        if (data.hasOwnProperty(k)) {
+          var ev = data[k]
+          console.log("Event",
+            ev.summary,
+            'is in',
+            ev.location,
+            'on the', ev.start.getDate(), 'of', months[ev.start.getMonth()]);
+        }
+      }
+    });
+  console.log(ical);
